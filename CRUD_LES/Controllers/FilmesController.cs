7990521a8517c511ddb76030d08c2c7713bc97ec
Gabriel <gabriel.reisleal@gmail.com>
2019-01-28 17:47:13 -0200

    Initial commit

---
+++ CRUD_LES/Controllers/FilmesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD_LES.Models;
+
+namespace CRUD_LES.Controllers
+{
+    public class FilmesController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Filmes
+        public ActionResult Index()
+        {
+            return View(db.Filmes.ToList());
+        }
+
+        // GET: Filmes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Filme filme = db.Filmes.Find(id);
+            if (filme == null)
+            {
+                return HttpNotFound();
+            }
+            return View(filme);
+        }
+
+        // GET: Filmes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Filmes/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Genero,Data,Estudio")] Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Filmes.Add(filme);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(filme);
+        }
+
+        // GET: Filmes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Filme filme = db.Filmes.Find(id);
+            if (filme == null)
+            {
+                return HttpNotFound();
+            }
+            return View(filme);
+        }
+
+        // POST: Filmes/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Genero,Data,Estudio")] Filme filme)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(filme).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(filme);
+        }
+
+        // GET: Filmes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Filme filme = db.Filmes.Find(id);
+            if (filme == null)
+            {
+                return HttpNotFound();
+            }
+            return View(filme);
+        }
+
+        // POST: Filmes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Filme filme = db.Filmes.Find(id);
+            db.Filmes.Remove(filme);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CRUD_LES/Models/Filme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_LES.Models
+{
+    public class Filme
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo genero é obrigatório.")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "O campo data é obrigatório.")]
+        public string Data { get; set; }
+
+        [Required(ErrorMessage = "O campo estudio é obrigatório.")]
+        public string Estudio { get; set; }
+    }
+}
